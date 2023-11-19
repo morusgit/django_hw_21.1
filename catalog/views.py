66018191c6dc40663be11fc2dfe5e0170d4bec60
Maxim Verbitskiy <morus@bk.ru>
2023-11-19 21:00:20 +0300

    Fixes

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'catalog/index.html')
+def index(request):  # Отображаем шаблон
+    return render(request, 'catalog/index.html')  # Отображаем шаблон
 
 
 def contacts(request):
-    if request.method == 'POST':
+    if request.method == 'POST':  # Проверяем, была ли отправлена форма
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'{name}, {email}, {message}')
-    return render(request, 'catalog/contacts.html')
-
-
-from django.shortcuts import render
-
-# Create your views here.
+    return render(request, 'catalog/contacts.html')  # Отображаем шаблон
