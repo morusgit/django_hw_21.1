0186e36d29d6359c274a92f99ee154a23382afa6
Maxim Verbitskiy <morus@bk.ru>
2023-12-02 12:31:26 +0300

    error fixed

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,4 @@ from catalog.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-    path('', include('catalog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
